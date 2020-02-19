1af367587fe64e15f6cee59be8671938dad0057a
Tomasz Zając <tomezpl@gmail.com>
2020-02-19 23:00:43 +0000

    Adds GLM and renders first triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,54 @@
+// C++ std libs
+#include <vector>
+#include <fstream>
+#include <string>
+
+// OpenGL includes
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <GL/GL.h>
+
+// GL math
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+
+glm::mat4 camera(float Translate, glm::vec2 const& Rotate)
+{
+	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.f);
+	glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -Translate));
+	View = glm::rotate(View, Rotate.y, glm::vec3(-1.0f, 0.0f, 0.0f));
+	View = glm::rotate(View, Rotate.x, glm::vec3(0.0f, 1.0f, 0.0f));
+	glm::mat4 Model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
+	return Projection * View * Model;
+}
+
+// Utility: returns contents of a file as text. Used to get shader sources.
+std::string getFileContents(std::string path)
+{
+	std::ifstream fs(path);
+	std::string ret = "";
+	while (fs.good())
+	{
+		std::string currentLine = "";
+		std::getline(fs, currentLine);
+		ret += currentLine;
+		if (!fs.eof())
+			ret += "\n";
+	}
+
+	return ret;
+}
 
 int main(int* argc, char** argv)
 {
 	// GLFW initialisiation
 	glfwInit();
+
+	// Set created contexts to use OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
 	// Window creation
 	GLFWwindow* window = glfwCreateWindow(1280, 720, "Pinball Game", nullptr, nullptr);
@@ -15,23 +58,54 @@ int main(int* argc, char** argv)
 	// Assign GL context before working with GL
 	glfwMakeContextCurrent(window);
 
+	glewExperimental = true;
 	// GLEW intialisation (has to be done once GL context is assigned)
 	glewInit();
 
 	glViewport(0, 0, 1280, 720);
 	bool running = true;
 
-	float verts[] = { 0.5f, 0.5f, 1.f };
+	// Vertices
+	std::vector<float> verts = {
+		-0.5f, -0.5f, 1.f, // bottom-left
+		0.5f, -0.5f, 1.f, // bottom-right
+		0.0f, 0.5f, 1.f // top
+	};
 
 	GLuint vbo;
 	GLuint vao;
 	glGenBuffers(1, &vbo);
 	glGenVertexArrays(1, &vao);
+
 	glBindVertexArray(vao);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, 3 * sizeof(float), verts, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glBufferData(GL_ARRAY_BUFFER, verts.size() * sizeof(float), verts.data(), GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (GLvoid*)0);
+	glEnableVertexAttribArray(0);
 	glBindVertexArray(0);
+
+	// Shaders
+	GLuint vertShader, fragShader;
+	vertShader = glCreateShader(GL_VERTEX_SHADER);
+	fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+	std::string vertShaderSrc = getFileContents("GLSL/Unlit.vert");
+	const char* vertShaderCStr = vertShaderSrc.c_str();
+	int vertShaderLength = vertShaderSrc.length();
+	glShaderSource(vertShader, 1, &vertShaderCStr, &vertShaderLength);
+	std::string fragShaderSrc = getFileContents("GLSL/Unlit.frag");
+	const char* fragShaderCStr = fragShaderSrc.c_str();
+	int fragShaderLength = fragShaderSrc.length();
+	glShaderSource(fragShader, 1, &fragShaderCStr, &fragShaderLength);
+
+	GLuint program;
+	program = glCreateProgram();
+	glAttachShader(program, vertShader);
+	glAttachShader(program, fragShader);
+	glCompileShader(vertShader);
+	glCompileShader(fragShader);
+	glLinkProgram(program);
+
+	//glBindAttribLocation(program, 0, "position");
 
 	while (running)
 	{
@@ -47,7 +121,11 @@ int main(int* argc, char** argv)
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glPointSize(10.0f);
-		glDrawArrays(GL_POINTS, 0, 1);
+		glUseProgram(program);
+		glBindVertexArray(vao);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glDrawArrays(GL_TRIANGLES, 0, verts.size() / 3);
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 	}
